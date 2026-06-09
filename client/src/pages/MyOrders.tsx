022a2f3be14a2b07793d2d7093d519695a2d9855
CharlesOsang017 <charlesosango02@gmail.com>
2026-06-09 20:09:24 +0300

    Add myOrders page

--- client/src/pages/MyOrders.tsx
+++ client/src/pages/MyOrders.tsx
@@ -1,7 +1,141 @@
-const MyOrders = () => {
-  return (
-    <div>MyOrders</div>
-  )
-}
+import { useEffect, useState } from "react";
+import type { Order } from "../types";
+import { Link, useSearchParams } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { dummyDashboardOrdersData, statusColors } from "../assets/assets";
+import Loading from "../components/Loading";
+import { Calendar, ChevronRight, Package } from "lucide-react";
 
-export default MyOrders
+const MyOrders = () => {
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
+
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState("all");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const tabs = ["all", "Placed", "Out for Delivery", "Delivered"];
+  const { clearCart } = useCart();
+
+  const fetchOrders = async () => {
+    setOrders(dummyDashboardOrdersData as any);
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (searchParams.get("clearCart")) {
+      clearCart();
+      setSearchParams({});
+      setTimeout(() => {
+        fetchOrders();
+      }, 2000);
+    } else {
+      fetchOrders();
+    }
+  }, [activeTab]);
+  return (
+    <div className="min-h-screen bg-app-cream mb-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1 className="text-2xl font-semibold text-app-green mb-6">
+          My Orders
+        </h1>
+        {/* Tabs */}
+        <div className="flex gap-2 mb-6 overflow-x-auto pb-2">
+          {tabs.map((tab) => (
+            <button
+              key={tab}
+              onClick={() => setActiveTab(tab)}
+              className={`px-4 py-2 font-medium rounded-xl whitespace-nowrap transition-colors ${activeTab === tab ? "bg-app-green text-white" : "bg-white border border-app-green text-app-text-light hover:bg-app-cream"}`}
+            >
+              {tab === "all" ? "All Orders" : tab}
+            </button>
+          ))}
+        </div>
+        {/* Orders List */}
+        {loading ? (
+          <Loading />
+        ) : orders.length === 0 ? (
+          <div className="text-center py-16">
+            <Package className="size-16 text-app-border mx-auto mb-4" />
+            <h2 className="text-lg font-medium text-app-green mb-2">
+              No order yet
+            </h2>
+            <p className="text-sm text-app-text-light mb-4">
+              Start shopping to see your orders here
+            </p>
+            <Link
+              to="/products"
+              className="bg-app-green text-white px-6 py-3 rounded-xl hover:bg-app-green-dark transition-colors"
+            >
+              Start Shopping
+            </Link>
+          </div>
+        ) : (
+          <div className="space-y-4">
+            {orders.map((order) => (
+              <Link
+                key={order._id}
+                to={`/orders/${order._id}`}
+                className="block max-w-4xl bg-white rounded-2xl p-5 hover:shadow transition-all"
+              >
+                {/* order id, date & status */}
+                <div className="flex items-start justify-between mb-3">
+                  {/* Left */}
+                  <div>
+                    <p className="text-sm font-medium text-app-green">
+                      Order # {order._id.slice(0, 8).toUpperCase()}
+                    </p>
+                    <div className="flex items-center gap-2">
+                      <Calendar className="size-3 text-app-text-light" />
+                      <span className="text-xs text-app-text-light">
+                        {new Date(order.createdAt).toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "numeric",
+                          year: "numeric",
+                        })}
+                      </span>
+                    </div>
+                  </div>
+                  {/* Right */}
+                  <div className="flex items-center gap-2">
+                    <span
+                      className={`px-4 py-1 text-xs font-medium rounded-full ${statusColors[order.status] || "bg-gray-100 text-gray-700"}`}
+                    >
+                      {order.status}{" "}
+                    </span>
+                    <ChevronRight className="size-4 text-app-text-light" />
+                  </div>
+                </div>
+                {/* Item Thumbnail */}
+                <div className="flex items-center gap-2 mb-3">
+                  {order.items.slice(0, 4).map((item, i) => (
+                    <img
+                      key={i}
+                      src={item.image}
+                      alt={item.name}
+                      className="size-12 sm:size-16 rounded-lg object-cover border border-app-border"
+                    />
+                  ))}
+                  {order.items.length > 4 && (
+                    <div className="size-12 sm:size-16 rounded-lg bg-app-cream flex-center text-xs font-semibold text-app-text-light">
+                      +{order.items.length - 4}
+                    </div>
+                  )}
+                </div>
+                {/* Total items & price */}
+                <div className="flex justify-between items-center pt-3 text-sm">
+                  <span>{order.items.length} items</span>
+                  <span className="font-semibold text-app-green">
+                    {currency}
+                    {order.total.toFixed(2)}
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
