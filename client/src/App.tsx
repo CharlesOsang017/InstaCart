46ef2bf9122aebfa1632d51d6b7afb6545c7e787
CharlesOsang017 <charlesosango02@gmail.com>
2026-06-10 15:50:02 +0300

    Add admin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,40 +18,59 @@ import AdminProducts from "./pages/admin/AdminProducts";
 import AdminProductForm from "./pages/admin/AdminProductForm";
 import AdminOrders from "./pages/admin/AdminOrders";
 import AdminDeliveryPartners from "./pages/admin/AdminDeliveryPartners";
+import DeliveryLogin from "./pages/delivery/DeliveryLogin";
+import DeliveryLayout from "./pages/delivery/DeliveryLayout";
+import DeliveryDashboard from "./pages/delivery/DeliveryDashboard";
 
 const App = () => {
   return (
     <>
-      <Toaster position="bottom-right" toastOptions={{duration: 3000, style: {background: "#1B3022", color: "#fff", borderRadius: "12px", fontSize: "14px"}}} />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#1B3022",
+            color: "#fff",
+            borderRadius: "12px",
+            fontSize: "14px",
+          },
+        }}
+      />
       <Routes>
         {/* Auth pages - No Navbar/Footer */}
-        <Route  path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Main pages - with Navbar/Footer */}
 
         <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<ProductPage />} />
-        <Route path="search" element={<SearchResults />} />
-        <Route path="deals" element={<FlashDeals/>} />
-        {/* Protected routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route  path='checkout' element={<Checkout />}/>
-          <Route  path="orders" element={<MyOrders />}/>
-          <Route path='orders/:id' element={<OrderTracking />} />
-          <Route path="addresses" element={<Addresses />}/>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<ProductPage />} />
+          <Route path="search" element={<SearchResults />} />
+          <Route path="deals" element={<FlashDeals />} />
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<MyOrders />} />
+            <Route path="orders/:id" element={<OrderTracking />} />
+            <Route path="addresses" element={<Addresses />} />
+          </Route>
         </Route>
-        </Route>  
-      {/* ADMIN Pages */}
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<AdminDashboard />} />
-        <Route path="products" element={<AdminProducts />} />
-        <Route path="products/new" element={<AdminProductForm />} />
-        <Route path="products/:id/edit" element={<AdminProductForm />} />
-        <Route path="orders" element={<AdminOrders />} />
-        <Route path="delivery-partners" element={<AdminDeliveryPartners />} />
-      </Route>
+        {/* ADMIN Pages */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="products/new" element={<AdminProductForm />} />
+          <Route path="products/:id/edit" element={<AdminProductForm />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="delivery-partners" element={<AdminDeliveryPartners />} />
+        </Route>
+
+        {/* Delivery Partner pages */}
+        <Route path="/delivery/login" element={<DeliveryLogin />} />
+        <Route path="/delivery" element={<DeliveryLayout />} />
+        <Route index element={<DeliveryDashboard />} />
       </Routes>
     </>
   );
