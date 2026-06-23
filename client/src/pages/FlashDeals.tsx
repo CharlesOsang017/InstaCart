70ac6763b8703786d53ea46608d51906802ef94f
CharlesOsang017 <charlesosango02@gmail.com>
2026-06-23 07:45:32 +0300

    Fetched flash deals products

--- client/src/pages/FlashDeals.tsx
+++ client/src/pages/FlashDeals.tsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../types";
-import { dummyProducts } from "../assets/assets";
 import { Zap } from "lucide-react";
 import Loading from "../components/Loading";
 import ProductCard from "../components/ProductCard";
+import api from "../config/api";
+import toast from "react-hot-toast";
 
 const FlashDeals = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setProducts(dummyProducts.filter((p: any) => p.stock > 0));
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get("/products/flash-deals");
+        setProducts(response.data.products);
+      } catch (error: any) {
+        toast.error(error.response?.data?.message || "Failed to fetch products");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
   }, []);
   return (
     <div className="min-h-screen bg-app-cream">
